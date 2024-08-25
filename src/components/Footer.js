12be03da39d32a5d39526391c3dd456dea199d61
Taimoor <149140170+ranataimoor067@users.noreply.github.com>
2024-08-25 20:53:05 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className="flex items-center lg:py-2">
           Built With <span className="text-primary dark:text-primaryDark text-2xl px-1">&#9825;</span>
           <Link href="/" className="underline underline-offset-2">
-            By Brian
+            By Taimoor
           </Link>
         </div>
         <Link
